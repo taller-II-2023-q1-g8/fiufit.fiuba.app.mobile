6ea3f9855aede0d08a250f77946223d59a6528a0
Ana Gabriela Gutson <agutson@fi.uba.ar>
2023-04-10 01:13:02 -0300

    add register screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login';
+// import Login from './components/Login';
+import Register from './screens/Register';
 
 export default function App() {
   return (
-    <Login />
+    <Register />
   );
 }
--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
+export const nameFieldType = "text";
 export const emailFieldType = "email-address";
 export const passwordFieldType = "default";
 
 export const EMAIL_FIELD_KEY = "email";
+export const NAME_FIELD_KEY = "name";
 export const PASSWORD_FIELD_KEY = "password";
