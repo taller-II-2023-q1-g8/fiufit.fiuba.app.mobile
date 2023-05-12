209c8c2edf9ee41735c97e05a6c6a38e46650b26
santiagobiach <santi.biach12@gmail.com>
2023-05-12 17:01:05 -0300

    when you enter trainer home it fetches your created plans(hardcoded right now)

--- screens/TrainerHome/index.js
+++ screens/TrainerHome/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
 import { useStateValue } from '../../utils/state/state';
+import { fetchUsersByUsername } from '../../requests';
 
 import TrainerHome from './layout';
 
 export default function TrainerHomeScreen() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [state, dispatch] = useStateValue();
+  useEffect(() => {
+    async function fetchData() {
+      // Deberiamos hacer un fetch de los training plans del trainer.
+      setData([
+        { title: 'Plan de la Fiuba', difficulty: 'EASY' },
+        { title: 'Road To Ingeniero', difficulty: 'MEDIUM' },
+        { title: 'Duro como final de AM3', difficulty: 'HARD' },
+        { title: 'Fuerte como el café del comedor', difficulty: 'MEDIUM' }
+      ]);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+  const handleItemPress = (planTitle) => {
+    console.log(planTitle);
+  };
   const handleTrainerHome = () => {
     dispatch({
       type: 'changeCurrentStack',
       newScreen: true
     });
   };
-  return <TrainerHome username={state.user.username} handleTrainerHome={handleTrainerHome} />;
+  return (
+    <TrainerHome
+      username={state.user.username}
+      handleTrainerHome={handleTrainerHome}
+      data={data}
+      handleItemPress={handleItemPress}
+      loading={loading}
+    />
+  );
 }
