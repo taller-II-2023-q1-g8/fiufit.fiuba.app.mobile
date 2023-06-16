f4bb4aadd641a2722ef53d0b4dc69aca13dd370f
Ana Gabriela Gutson <agutson@fi.uba.ar>
2023-06-16 02:36:38 -0300

    add button to share goals

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import 'expo-dev-client';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import messaging, { firebaseMessaging } from '@react-native-firebase/messaging';
+import { Alert, Share } from 'react-native';
 
 import './firebaseConfig';
 import { StateProvider } from './src/state';
@@ -15,10 +16,29 @@ if (__DEV__) {
   });
 }
 
+const shareMessage = async (message) => {
+  try {
+    await Share.share({
+      message: `Completé una meta en FiuFit: ${message.split(':')[1]}!`
+    });
+  } catch (error) {
+    console.log('Error al compartir:', error);
+  }
+};
+
 async function onMessageReceived(message) {
   if (!message.notification.title.startsWith('Mensaje de')) {
+    console.log({ message });
     console.log('NOTIFICATION RECEIVED:', message.title);
-    alert(message.notification.body);
+    Alert.alert('Meta completada!', message.notification.body, [
+      {
+        text: 'Compartir',
+        onPress: () => shareMessage(message.notification.body)
+      },
+      {
+        text: 'Ok'
+      }
+    ]);
   }
 }
 
