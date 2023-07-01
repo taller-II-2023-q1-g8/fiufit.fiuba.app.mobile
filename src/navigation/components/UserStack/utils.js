df4d33cf3094471404c26fcd3034ff50f1b729ac
santiagobiach <santi.biach12@gmail.com>
2023-07-01 15:01:56 -0300

    Better tab bar

--- src/navigation/components/UserStack/utils.js
+++ src/navigation/components/UserStack/utils.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import texts from '../../../texts';
 import ICONS from '../../constants';
+import { colors } from '../../../colors';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const styles = StyleSheet.create({
+  walkingContainerStyle: {
+    height: 80,
+    width: 80,
+    backgroundColor: colors.main_soft,
+    borderRadius: 100,
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export const tabBarIconsAthlete = (route, { focused, color, size }) => {
   let iconName;
-
   if (route.name === texts.Home.name) iconName = focused ? ICONS.HOME : ICONS.FOCUSED_HOME;
   else if (route.name === texts.SearchUsersStack.name) iconName = focused ? ICONS.LIST : ICONS.FOCUSED_LIST;
   else if (route.name === texts.SearchPlansStack.name) iconName = focused ? ICONS.LIST : ICONS.FOCUSED_LIST;
-  else if (route.name === texts.Walking.name) iconName = focused ? ICONS.WALKING : ICONS.FOCUSED_WALKING;
-  else if (route.name === texts.AddPlan.name) {
+  else if (route.name === texts.Walking.name) {
+    const color1 = focused ? colors.drawer : color;
+    return (
+      <View style={styles.walkingContainerStyle}>
+        <MaterialCommunityIcons name="shoe-print" size={30} color={color1} />
+      </View>
+    );
+  } else if (route.name === texts.AddPlan.name) {
     iconName = focused ? ICONS.ADD : ICONS.FOCUSED_ADD;
     color = 'red';
   } else if (route.name === texts.UserProfileStack.name)
