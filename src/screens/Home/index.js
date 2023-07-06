275860c17c3c10f125d9ed6e827093ecbe28e2d7
santiagobiach <santi.biach12@gmail.com>
2023-07-05 23:38:51 -0300

    Fix updates ultimos planes realizados

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -21,6 +21,8 @@ export default function HomeScreen({ navigation }) {
   const [loading, setLoading] = useState(false);
   const [state, dispatch] = useStateValue();
   const [goals, setGoals] = useState(state.userGoals);
+  const [suggestedPlans, setSuggestedPlans] = useState([]);
+  const [lastPlans, setLastPlans] = useState([]);
 
   useEffect(() => {
     messaging()
@@ -31,13 +33,45 @@ export default function HomeScreen({ navigation }) {
     updateLoginTime(state.user.username);
   }, []);
   useEffect(() => {
+    const fetchLastPlans = async () => {
+      const response = await fetchPlans('');
+      const plans = await response.json();
+      const completedPlansResponse = await fetchCompletedPlanMetricsByUsername(state.user.username);
+      const completedPlans = await completedPlansResponse.json();
+      const aux = completedPlans.message;
+      aux.sort((plan1, plan2) => new Date(plan2.created_at) - new Date(plan1.created_at));
+      const unique = [];
+      const aux2 = [];
+      aux.forEach((plan) => {
+        if (!unique.includes(plan.plan_title)) {
+          unique.push(plan.plan_title);
+          aux2.push(plan);
+        }
+      });
+      aux2.splice(3);
+      const lastPlansItems = [];
+      aux2.forEach((plan) => {
+        const planData = plans.find((data) => data.title === plan.plan_title);
+        if (planData !== undefined) {
+          lastPlansItems.push({
+            type: 'training_plan_completed',
+            title: plan.plan_title,
+            plan: planData,
+            difficulty: planData.difficulty,
+            tags: planData.tags,
+            id: planData.id,
+            date: plan.created_at
+          });
+        }
+      });
+      setLastPlans(lastPlansItems);
+    };
     if (!loading) {
       const g = state.userGoals;
       setGoals(g);
+      fetchLastPlans();
     }
   }, [state.userGoals]);
-  const [suggestedPlans, setSuggestedPlans] = useState([]);
-  const [lastPlans, setLastPlans] = useState([]);
 
   function avgCalification(plan) {
     const { athletes } = plan;
